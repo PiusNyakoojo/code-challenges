ac4cd4cd33c554748330cfe194a51f660a87759d
PiusNyakoojo <pnyakoojo.16@gmail.com>
2016-09-20 00:35:23 -0500

    Fixed styling issues and updated webpack config

--- ui-transition/webpack.config.js
+++ ui-transition/webpack.config.js
@@ -1,17 +1,21 @@
 let path = require('path');
 
 let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    context: path.resolve('app/src'),
-    entry: ['./main.js'],
+    context: path.resolve('app'),
+    entry: ['./src/main.js'],
     output: {
         path: path.resolve('dist'),
         publicPath: '',
         filename: 'bundle.js'
     },
     plugins: [
-		new ExtractTextPlugin('styles.css')
+		new ExtractTextPlugin('styles.css'),
+        new CopyWebpackPlugin([
+            { from: 'assets/images', to: 'assets/images' }
+        ])
 	],
     devServer: {
         contentBase: 'app'
@@ -34,7 +38,17 @@ module.exports = {
 				test: /\.scss$/,
 				exclude: /node_modules/,
 				loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader!sass-loader")
-			}
+			},
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader")
+            },
+            {
+                test: /\.jpe?g$|\.gif$|\.png$|\.svg$/,
+                exclude: /node_modules/,
+                loader: "file"
+            }
         ]
     },
     resolve: {
